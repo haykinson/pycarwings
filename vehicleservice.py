382ef17fca5c1482633635050f805baa5080a1b4
Ilya Haykinson <haykinson@gmail.com>
2011-09-25 21:31:30 -0700

    add support for charging

--- vehicleservice.py
+++ vehicleservice.py
@@ -1,6 +1,7 @@
 from xml.dom import minidom
 import time
 import sha
+from datetime import datetime
 
 from connection import Connection, AuthException
 from xmlhelper import dict_to_xml
@@ -27,4 +28,20 @@ class VehicleService(object):
         result = self.connection.post_xml(self.SERVICE_PATH, xml, True)
         return True
     
-        
+    def start_charge(self, vin):
+        if not self.connection.logged_in:
+            raise AuthException('Please log in before starting a charge')
+
+        namespaces = {'ns4':'urn:com:airbiquity:smartphone.vehicleservice:v1',
+                      'ns3':'urn:com:hitachi:gdc:type:vehicle:v1',
+                      'ns2':'urn:com:hitachi:gdc:type:portalcommon:v1'}
+
+        d = {'ns3:BatteryRemoteChargingRequest':
+                 {'@type':'vehicle:BatteryRemoteChargingRequestType',
+                  'ns3:VehicleServiceRequestHeader':
+                      {'ns2:VIN': vin},
+                  'ns3:NewBatteryChargeRequest':
+                      {'ns3:ExecuteTime', datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}}}
+        xml = dict_to_xml(d, 'ns4:SmartphoneRemoteBatteryChargeRequest', namespaces)
+        result = self.connection.post_xml(self.SERVICE_PATH, xml, True)
+        return True
